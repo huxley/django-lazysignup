c1a785c3aea05a6fda41e476e8343ea1c76aeca0
Patrick Taylor <patricktaylor@mac.com>
2013-02-11 12:39:15 -0400

    Update lazysignup/urls.py
    Deprecated in Django 1.5

--- lazysignup/urls.py
+++ lazysignup/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
+
 from django.views.generic.simple import direct_to_template
 
 # URL patterns for lazysignup
